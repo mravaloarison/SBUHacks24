14865b44802f738fa486ad6d358a277c8d670309
NelsonWang <wangnelson2@gmail.com>
2024-02-10 17:17:24 -0500

    uathand -rotected pages

--- src/App.js
+++ src/App.js
@@ -2,33 +2,60 @@ import { Container } from 'react-bootstrap';
 import { Recording } from './components/Recording';
 import { NavBarHeader } from './components/NavBarHeader';
 import { LandingPage } from './components/LandingPage';
-import {ProfilePage} from './screens/ProfilePage';
-import {DashboardPage} from './screens/DashboardPage';
+import { ProfilePage } from './screens/ProfilePage';
+import { DashboardPage } from './screens/DashboardPage';
 
 import './global.css';
 
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ user, children }) => {
+  console.log(user, 'user')
+  if (!user) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
 
 function App() {
-  // const userName = sessionStorage.getItem('user');
+  const userName = sessionStorage.getItem('user');
+  console.log('======================')
+  console.log(userName, 'userName')
 
   return (
     <div className="App">
       <Router>
-        <NavBarHeader  className="clickeable" />
+        <NavBarHeader className="clickeable" />
         <Container fluid className='mx-auto col-md-8'>
           <Routes>
             <Route path="/" element={<LandingPage />} />
-            <Route path="/interview" element={<Recording />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route
+              path="interview"
+              element={
+                <ProtectedRoute user={userName}>
+                  <Recording />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/profile" element={
+              <ProtectedRoute user={userName}>
+                <ProfilePage />
+              </ProtectedRoute>
+            } />
+            <Route path="/dashboard" element={
+              <ProtectedRoute user={userName}>
+                <DashboardPage />
+              </ProtectedRoute>
+            } />
           </Routes>
         </Container>
 
         <footer className="py-3" style={{ position: "absolute", bottom: 0, width: "100vw" }}>
           <Container className='text-center'>
-            <small className="text-muted">&copy; 2024 by Us</small>
+            <small className="text-muted">&copy; 2024 by SBU Hacks</small>
           </Container>
         </footer>
       </Router>
--- src/authentication.js
+++ src/authentication.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOonudCmYXaeuKvJ3BYhiHH2Q6986s7yU",
-  authDomain: "ghw23-85c04.firebaseapp.com",
-  projectId: "ghw23-85c04",
-  storageBucket: "ghw23-85c04.appspot.com",
-  messagingSenderId: "72534743572",
-  appId: "1:72534743572:web:71440b2fc58cbb8397e501"
+    apiKey: "AIzaSyAOonudCmYXaeuKvJ3BYhiHH2Q6986s7yU",
+    authDomain: "ghw23-85c04.firebaseapp.com",
+    projectId: "ghw23-85c04",
+    storageBucket: "ghw23-85c04.appspot.com",
+    messagingSenderId: "72534743572",
+    appId: "1:72534743572:web:71440b2fc58cbb8397e501"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -16,23 +16,26 @@ const provider = new GoogleAuthProvider();
 const auth = getAuth(app);
 
 export const signInWithGoogle = (navigate) => {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      const user = result.user;
-      sessionStorage.setItem('user', user.displayName);
-      
-      // Redirect to a different route
-      navigate("/dashboard");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            const user = result.user;
+            sessionStorage.setItem('user', user.displayName);
+
+            // Redirect to a different route
+            //   Reload the page
+            window.location.reload();
+            navigate("/dashboard");
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 export const signOut = (navigate) => {
-  sessionStorage.removeItem('user');
-  auth.signOut();
+    sessionStorage.removeItem('user');
 
-  // Redirect to a different route
-  navigate("/");
+    auth.signOut();
+
+    // Redirect to a different route
+    navigate("/");
 }
